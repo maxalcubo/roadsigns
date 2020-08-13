c7e4e92e8ad6891fae8907ca6d438d70975dafe0
Amilcar Maximo Laura Canaza <alaurac@unsa.edu.pe>
2020-08-12 22:38:51 -0500

    Primer commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalMove;
+    private float verticalMove;
+    public CharacterController player;
+    public float velocidadGiro;
+    public float velocidadAvance;
+
+    public AudioSource sonidoMotor;
+    //public float velocidadGiro = 40f;
+    //public float velocidadAvance = 0.2f;
+
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+        if(horizontalMove == 0 && verticalMove == 0){
+            sonidoMotor.Pause();
+        }else{
+            sonidoMotor.UnPause();
+        }
+    }
+    private void FixedUpdate() {
+        player.Move(transform.forward * Time.deltaTime * verticalMove * velocidadAvance);
+        transform.Rotate(Vector3.up * horizontalMove * velocidadGiro * Time.deltaTime);
+
+        Vector3 moveVector = Vector3.zero;
+        if (player.isGrounded == false)
+        {
+            moveVector += Physics.gravity;
+        }
+        player.Move(moveVector * Time.deltaTime);
+    }
+}
+
---
+++ Assets/Scripts/PuntosJugador.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PuntosJugador : MonoBehaviour
+{
+    public AudioSource point;
+    public Text marcador;
+    public Text terminaste;
+    int contador;
+
+    void Awake()
+    {
+        contador = 0;
+        ActualizarMarcador();
+        //terminaste.gameObject.SetActive(false);
+        
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        point.Play();
+
+        Destroy(other.gameObject);
+        contador = contador + 1;
+        ActualizarMarcador();
+        if(contador >= 13)
+        {
+            //terminaste.gameObject.SetActive(true);
+        }
+    }
+
+    void ActualizarMarcador()
+    {
+        marcador.text = "Puntos: " + contador;
+    }
+}
